05cbe42ff9e2fdae786ca01f7501994fa1250ac3
moody94 <moody.kh.022@gmail.com>
2022-07-28 00:09:26 +0200

    kmom1

---
+++ tests/Feature/UserRegisterControllerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserRegisterControllerTest extends TestCase
+{
+    // use RefreshDatabase;
+
+    //     /**
+    //  * Test register method succeeds on
+    //  * filling correct input data
+    //  *
+    //  * @return void
+    //  */
+    // public function testRegisterSuccess()
+    // {
+    //     $user = [
+    //         'name' => 'John Doe',
+    //         'email' => 'johndoe@test.com',
+    //         'password' => 'passwordwith8characters',
+    //         'password_confirmation' => 'passwordwith8characters'
+    //     ];
+
+    //     $response = $this->json('POST', '/register', $user);
+
+    //     $response->assertStatus(201);
+    // }
+
+    // /**
+    //  * Test register method fails when
+    //  * filling incorrect input data
+    //  *
+    //  * @return void
+    //  */
+    // public function testRegisterFail()
+    // {
+    //     $user = [
+    //         'name' => 'John Doe',
+    //         'email' => 'johndoe',
+    //         'password' => 'secret',
+    //         'password_confirmation' => 'secret'
+    //     ];
+
+    //     $response = $this->json('POST', '/register', $user);
+
+    //     $response->assertStatus(422);
+    // }
+}
