2540508011ec3015f464e06a31b8000138553910
moody94 <moody.kh.022@gmail.com>
2022-07-28 00:50:59 +0200

    kmom0

---
+++ tests/Feature/FeatureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Featue;
+
+use PHPUnit\Framework\TestCase;
+
+class FeatureTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
